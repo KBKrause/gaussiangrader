dc5abe5d7d232cd6fcfe93632727eea078eb10c9
Kevin Krause <kkrause2@carthage.edu>
2018-04-24 11:23:28 -0700

    Session key

--- index.aspx.cs
+++ index.aspx.cs
@@ -9,7 +9,6 @@ public partial class Index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        System.Diagnostics.Debug.Print((string)Session["username"]);
-        System.Diagnostics.Debug.Print((string)Session["email"]);
+        // If Session[key] is not valid or has not been set, it returns null.
     }
 }

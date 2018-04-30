85b76ea8feb9bc3c8704c20f4119af8dd85ad09d
TravisD42 <tdillard@carthage.edu>
2018-04-30 12:11:37 -0500

    User can now Import Excel Spreadsheet and display it in HTML

---
+++ SheetJS/importExcel.js
@@ -0,0 +1,15 @@
+;
+
+$('#inputExcel').change(function(e){
+    var reader = new FileReader();
+
+    reader.readAsArrayBuffer(e.target.files[0]);
+
+    reader.onload = function(e) {
+        var data = new Uint8Array(reader.result);
+        var wb = XLSX.read(data, {type: 'array'});
+
+        var htmlstr = XLSX.write(wb, {sheet: 'sheet num1', type: 'binary', bookType: 'html'});
+        $('#wrapper')[0].innerHTML += htmlstr;
+    }
+});
